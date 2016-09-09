1c56ecc335a3021805703f7f294d3ebbebe713cc
Trevor Hiley <trevorhiley@gmail.com>
2016-09-09 01:15:25 +0000

    initial commit

---
+++ truffle.js
@@ -0,0 +1,18 @@
+module.exports = {
+  build: {
+    "index.html": "index.html",
+    "pretty.html": "pretty.html",
+    "app.js": [
+      "javascripts/app.js"
+    ],
+    "app.css": [
+      "stylesheets/app.css"
+    ],
+    "images/": "images/",
+    "fonts/": "fonts/"
+  },
+  rpc: {
+    host: "etherhack-trevorhiley.c9users.io",
+    port: 8081
+  }
+};
